03d215bbf47f58a4ae924ad4e20c6ec1a3f2a4e1
Jason Collins <the.oem@gmail.com>
2017-05-10 19:36:09 -0600

    Refactor some of game out into a Grid class.

---
+++ test/game-test.js
@@ -0,0 +1,10 @@
+import { assert } from 'chai'
+import { expect } from 'chai'
+import GamePiece from '../lib/GamePiece.js'
+import Bike from '../lib/Bike.js'
+import Grid from '../lib/Grid.js'
+
+describe('Game testing occurs here...', () => {
+
+
+})
